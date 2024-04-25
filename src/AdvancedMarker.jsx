f4296c6f194edb69bf76610230382e41180bc7ff
jdrichards-pursuit <jdrpursuit@gmail.com>
2024-04-25 18:20:36 -0400

    add vis.gl maps

---
+++ src/AdvancedMarker.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+function AdvancedMarker({ places }) {
+  const [selectedPlace, setSelectedPlace] = useState(null);
+  const [marker, setMarker] = useState(null);
+  // const [markerRef, marker] = useAdvancedMarkerRef();
+  const [infowindowOpen, setInfowindowOpen] = useState(false);
+
+  return (
+    <div>
+      {" "}
+      {places.map((place) => (
+        <div key={place.place_id}>
+          <AdvancedMarker
+            ref={marker}
+            position={{
+              lat: place.geometry.location.lat,
+              lng: place.geometry.location.lng,
+            }}
+            title={place.name}
+            onClick={() => {
+              setSelectedPlace(place);
+              setInfowindowOpen(true);
+            }}
+          >
+            {/* <div
+              style={{
+                width: 20,
+                height: 16,
+                position: "absolute",
+                top: 0,
+                left: 0,
+                background: "#1dbe80",
+                border: "2px solid #0e6443",
+                borderRadius: "50%",
+                transform: "translate(-50%, -50%)",
+              }}
+            ></div> */}
+            <Pin
+              background={"purple"}
+              borderColor={"black"}
+              glyphColor={"#0f677a"}
+            ></Pin>
+          </AdvancedMarker>
+          {infowindowOpen && (
+            <InfoWindow
+              anchor={setMarker}
+              maxWidth={200}
+              position={{
+                lat: selectedPlace.geometry.location.lat + 0.002,
+                lng: selectedPlace.geometry.location.lng + 0.0001,
+              }}
+              onCloseClick={() => {
+                setSelectedPlace(null);
+                setInfowindowOpen(false);
+              }}
+            >
+              {selectedPlace.name}
+            </InfoWindow>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default AdvancedMarker;
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const App = () => {
-  return <div>Basic Pursuit React Starter</div>;
-};
+import {
+  APIProvider,
+  Map,
+  AdvancedMarker,
+  Pin,
+  InfoWindow,
+  useAdvancedMarkerRef,
+} from "@vis.gl/react-google-maps";
 
+import AdvancedMarker from "./AdvancedMarker";
+
+function App() {
+  const { VITE_API_KEY } = import.meta.env;
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      const response = await axios.get(
+        "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
+        {
+          params: {
+            key: VITE_API_KEY,
+            radius: 5000,
+            location: " 48.864716,2.349014",
+            type: "restaurant", // Forward query parameters from the client to the Google Maps API
+          },
+        }
+      );
+      setPlaces(response.data.results);
+      console.log(response.data);
+    };
+    fetchPlaces();
+  }, []);
+
+  return (
+    <APIProvider apiKey={VITE_API_KEY}>
+      <Map
+        mapId={"2a1653e64c33bca"}
+        style={{ width: "100vw", height: "100vh" }}
+        defaultCenter={{ lat: 48.864716, lng: 2.349014 }}
+        defaultZoom={13}
+        gestureHandling={"greedy"}
+        disableDefaultUI={true}
+      />
+      <AdvancedMarker places={places} />
+    </APIProvider>
+  );
+}
 export default App;
